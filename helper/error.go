63f560a995742f463e0bc5ed8f344589d7d3c10d
Rizky Novando Priyadi <rizkynovando@gmail.com>
2024-03-26 00:32:00 +0700

    update error response

--- helper/error.go
+++ helper/error.go
@@ -2,5 +2,7 @@ package helper
 
 func Error(err error, errCode string) map[string]interface{} {
 	LoggerErr(err.Error())
-	return Response(errCode, nil, -1)
+	return Response(errCode, map[string]interface{}{
+		"error": err.Error(),
+	}, -1)
 }
